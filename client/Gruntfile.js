5f026de7908718b0269a915680499b069bb72470
Yandong Yao <yyao@pivotal.io>
2015-06-06 09:45:56 +0800

    update

--- client/Gruntfile.js
+++ client/Gruntfile.js
@@ -68,8 +68,8 @@ module.exports = function (grunt) {
             options: {
                 port: 9000,
                 // Change this to '0.0.0.0' to access the server from outside.
-                hostname: 'localhost',
-                debug: true,
+                hostname: '0.0.0.0',
+                // debug: true,
                 livereload: 35729
             },
             livereload: {
